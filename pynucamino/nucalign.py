b156ee2fd31160c6425af354c73501a0efd7589a
Nathaniel Knight <nknight@cfenet.ubc.ca>
2018-04-11 16:08:01 -0700

    Add profile and profile_genes methods to Nucamino object

--- pynucamino/nucalign.py
+++ pynucamino/nucalign.py
@@ -1,8 +1,9 @@
+import functools
 import json
 import subprocess
 
 
-class NucAlignment(object):
+class Nucamino(object):
 
     @classmethod
     def _nucalign(cls, inputseqs, profile, genes, check=True):
@@ -20,19 +21,67 @@ class NucAlignment(object):
         outp = align_proc.stdout.decode('utf8')
         return json.loads(outp), align_proc
 
-    def __init__(self, seq, profile, genes):
-        '''Perform and alignment and store the results.
+    def __init__(self, seqs, profile, genes, check=True):
+        '''Align a FASTA-formatted collection of nucleotide sequences and
+        return the results.
 
-        Arguments:
+        :param:`seq` is a FASTA-formatted collection of nucleotide
+        sequences to align.
 
-          - :param seq:`seq`: The sequence to align (as a FASTA formatted
-            string).
-          - :param profile:`profile`: The name of the alignment profile to use
-            while aligning (e.g. `hcv1a`, `hiv1b`).
-          - :param genes:`genes`: An iterable of genes to try aligning against.
+        :param:`profile` is the name of a built-in nucamino alignment
+        (which specifies the reference sequence and scoring parameters
+        used by the aligner). See :func:`profiles` for info on how to
+        get a list of available profiles.
+
+        :param:`genes` is an iterable of gene names to include in the
+        alignment. See :func:`profile_genes` for details on how to get
+        a list of available genes for each profile.
+
+        :param:`check` Indicates that the return-code of the alignment
+        process should be checked, and an exception raised if it's an
+        error code. The default is `True`.
         '''
         self.result, self.proc = self._nucalign(
-            seq,
+            seqs,
             gene=genes,
             profile=profile,
+            check=check,
         )
+
+    # `profiles` and `profile_genes` could almost be constants, but to
+    # track changes in the underlying nucamino binary, instead we
+    # interrogate the binary the first time they're accessed and cache
+    # the result.
+
+    @staticmethod
+    @functools.lru_cache(maxsize=1)
+    def profiles():
+        '''Returns a list of supported alignment profiles'''
+        command = ["./nucamino", "profile", "list"]
+        proc = subprocess.run(command, stdout=subprocess.PIPE)
+        proc.check_returncode()
+        outp = proc.stdout.decode('utf8')
+        return [
+            profile.strip()
+            for profile in outp.split('\n')
+            if profile.strip()
+        ]
+
+    @classmethod
+    @functools.lru_cache(maxsize=1)
+    def profile_genes(cls, profile):
+        cls._check_profile(profile)
+        command = ["./nucamino", "profile", "list-genes", profile]
+        proc = subprocess.run(command, stdout=subprocess.PIPE)
+        proc.check_returncode()
+        outp = proc.stdout.decode('utf8')
+        return [
+            gene.strip()
+            for gene in outp.split()
+            if gene.strip()
+        ]
+
+    @classmethod
+    def _check_profile(cls, profile):
+        if profile not in cls.profiles():
+            raise ValueError("Unknown profile '{}'".format(profile))
