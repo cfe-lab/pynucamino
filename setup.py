75840969a67c0c361b313b604cf8b42e0ee92ce5
Nathaniel Knight <nknight@cfenet.ubc.ca>
2018-04-12 16:39:17 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ import setuptools
 import pynucamino
 
 setuptools.setup(
+
     name="pynucamino",
     version=pynucamino.__version__,
-    packages=setuptools.find_packages(),
     author="Nathaniel Knight",
     author_email="nknight@cfenet.ubc.ca",
     description=(
@@ -14,7 +14,14 @@ setuptools.setup(
     ),
     license="MIT",
     url="https://github.com/hcv-shared/pynucamino",
-    python_requires=">=2.7,<3.7",
+
     test_suite="test",
+
+    python_requires=">=3.3,<3.7",
+    packages=setuptools.find_packages(),
+    install_requires=["setuptools"],
     zip_safe=False,
+
+    include_package_data=True,
+
 )
