f28a0e5edc02f1eda4f69101990fefd0605221a6
Nathaniel Knight <nknight@cfenet.ubc.ca>
2018-04-12 13:51:07 -0700

    Add align_file function

--- pynucamino/functions.py
+++ pynucamino/functions.py
@@ -1,7 +1,13 @@
+import contextlib
+import pathlib
+
 from .nucalign import Nucamino
 
 
-def align(*, seqs, profile, genes, check=True):
+__all__ = ("align", "align_file")
+
+
+def align(seqs=None, profile=None, genes=None, check=True):
     '''Align a fasta-formatted collection of nucleotide sequences using
     the given profile name and iterable of gene names
 
@@ -14,3 +20,20 @@ def align(*, seqs, profile, genes, check=True):
         check=check,
     )
     return alignment.result
+
+
+def align_file(input_file, profile=None, genes=None, check=True):
+    '''Align a fasta-formatted file of input sequences using the given
+    profile name and iterable of gene names.
+
+    :param:`input_file` can be a file object, a `string`, or a
+    `pathlib.Path`. Its contents will be passed to :class:`Nucamino`.
+    If it's a file, it will be closed.
+
+    See :class:`Nucamino` for details about the other arguments.
+    '''
+    if isinstance(input_file, str) or isinstance(input_file, pathlib.Path):
+        input_file = open(input_file, 'r')
+    with contextlib.closing(input_file) as inf:
+        seqs = inf.read()
+    return align(seqs=seqs, profile=profile, genes=genes, check=check)
